16f6c2e2d2b3979cffb1104c89639ceab60fc515
fatma khaled <fatmakhaled2022999@gmail.com>
2023-05-27 00:01:54 +0300

    author

--- models/author.js
+++ models/author.js
@@ -9,14 +9,15 @@ const authorSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Book'
   }]
-}, { timestamps: true });
+}, 
+{ timestamps: true });
 
 const authorModel = mongoose.model('Author', authorSchema);
 
 authorModel.create({
-         name: 'alaaEllol=>auther',
-         books:['646117922fa6eaa228189305']
-    });
+  name: 'alaaEllol=>auther',
+  books:['646117922fa6eaa228189305']
+});
 
 
 module.exports = authorModel;

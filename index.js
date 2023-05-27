42ebb2b5eadcc458bafca3148f2314f79954c5a4
Sameh <sameh-mahmoud@outlook.com>
2023-05-27 18:48:29 +0300

    last update

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose')
-const express = require('express')
-const app = express()
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bookRouter = require("./Routes/BooksRoute");
+// const users = require('./models/user')
+// const books = require('./models/book')
+// const orders = require('./models/order')
+// const authers = require('./models/auther')
 
-const users = require('./models/user')
-const books = require('./models/book')
-const orders = require('./models/order')
-const authers = require('./models/auther')
+var connectionString =
+  "mongodb+srv://ellol:vqnWuRTZlgZ7HhR4@alef-cluster.xt2vp4y.mongodb.net/alef-database";
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas successfully");
+  })
+  .catch((error) => console.error(error));
 
-var connectionString = 'mongodb+srv://ellol:vqnWuRTZlgZ7HhR4@alef-cluster.xt2vp4y.mongodb.net/alef-database';
-mongoose.connect(connectionString).then(() => {
-      console.log('Connected to MongoDB Atlas successfully');
-}).catch((error) => console.error(error));
-
-
-
-
-
-
-app.listen(3001,()=>{
-console.log('Server started successfully');
-})
+app.listen(3001, () => {
+  console.log("Server started successfully");
+});
+app.use(express.json())
+app.use("/books", bookRouter);
